f16d186c98820fbbd612f7c77d442ebdd534a326
Nicholas Terrell <nicholas@terrell.com.au>
2016-03-31 09:05:12 +1100

    updated example

--- example/search.js
+++ example/search.js
@@ -2,8 +2,12 @@ var iTunes = require(__dirname + "/../index.js");
 
 var it = new iTunes();
 
-it.search("young thug just might").then(function(results) {
-  console.log(results);
+it.search("young thug just might").then(function(resultData) {
+  // add larger image to results
+  resultData.results = it.addImages(resultData.results, 600);
+  // remove clean duplicates from results.
+  resultData.results = it.removeCleanDuplicates(resultData.results);
+  console.log(resultData);
 }).error(function(err) {
   console.log(err.message);
 });
